a8f880e0b79dd0b512113756b67515f1a251d2f9
johannpereze <sebas.jsp@gmail.com>
2021-09-02 20:26:55 -0500

    Clase 79

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -3,8 +3,5 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent{
-  
-}
+export class SidebarComponent {}
