93dc4d7a0a24ffef26a988d0684e5dcb1a52827a
johannpereze <sebas.jsp@gmail.com>
2021-09-04 15:26:01 -0500

    Clase 81. Mi solución de la tarea de volver funcional el historial

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
 
 @Component({
   selector: 'app-busqueda',
@@ -9,7 +10,10 @@ export class BusquedaComponent {
   @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>; //Va a buscar un elemento con la referencia dentro del paréntesis y se lo va a asignar a la variable que estamos inicializando al final de esta línea
   //el simbolo ! se llama non-null assertion operator y le dice a typescript que confíe en mí, que nunca se lo voy a mandar nulo
   buscar() {
-    console.log(this.txtBuscar.nativeElement.value);
+    const valor = this.txtBuscar.nativeElement.value
+    this.gifsService.buscarGifs(valor)
     this.txtBuscar.nativeElement.value = '';
   }
+
+  constructor(private gifsService: GifsService ){}
 }
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { GifsService } from '../../gifs/services/gifs.service';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
 })
-export class SidebarComponent {}
+export class SidebarComponent {
+  constructor(private gifsService: GifsService){}
+get historial():string[]{return this.gifsService.historial} //Esto lo hice generando un getter. No se puede hacer sin uno. Tiene que ser una función
+}
